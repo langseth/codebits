07773b75c10c9d7c03101bceb784b058d30f737d
Ryan Langseth <ryan.langseth@gmail.com>
2012-02-11 20:04:17 -0600

    changed the spacing on the file and adjusted some of the setup of printing the file tail. It will actually print out and do a proper 'tail -F' now.

--- pytail.py
+++ pytail.py
@@ -2,38 +2,52 @@
 
 import select
 from select import kqueue, kevent
+from time import sleep
 import os
 import sys
 
-filename = "foo.txt"
 
-
-fd = os.open(filename,os.O_RDONLY)
-
+filename = "access"
+fh = open(filename, "r")
 kq = kqueue()
-
-event = [kevent(fd, filter=select.KQ_FILTER_READ, flags=select.KQ_EV_ADD),
-         kevent(fd, filter=select.KQ_FILTER_VNODE, flags=select.KQ_EV_ADD | select.KQ_EV_CLEAR,
-	        fflags=select.KQ_NOTE_DELETE | select.KQ_NOTE_RENAME),
-	 kevent(1000,filter=select.KQ_FILTER_TIMER,
-	        flags=select.KQ_EV_ADD | select.KQ_EV_CLEAR, data=1000)]
-
-events = kq.control(event,0,5)
-
-while True:
-	print "loop"
-	r_events = kq.control(None,4)
-	for event in r_events:
-		print event
-	for event in r_events:
-		if event.fflags & select.KQ_NOTE_DELETE:
-			print "file was deleted"
-		elif event.fflags & select.KQ_NOTE_RENAME:
-			print "file was renamed"
-			event.flags = select.KQ_EV_CLEAR
-			kq.control([event],0,0)
-		elif event.ident == 1000:
-			event.flags = select.KQ_EV_DISABLE
-			kq.control([event],0,0)
-kq.close()
-os.close(fd)
+event = [
+         kevent(fh, filter=select.KQ_FILTER_VNODE,
+                flags=select.KQ_EV_ADD | select.KQ_EV_CLEAR,
+                fflags=select.KQ_NOTE_DELETE | 
+                       select.KQ_NOTE_RENAME |
+                       select.KQ_NOTE_WRITE)
+        ]
+print fh.read(),
+kq.control(event,0)
+try:
+    while True:
+        r_events = kq.control(None,1)
+        for event in r_events:
+            if event.fflags & select.KQ_NOTE_WRITE:
+                print fh.read(),
+            if event.fflags & select.KQ_NOTE_DELETE:
+                kq.close()
+                fh.close()
+                exit(0)
+            elif event.fflags & select.KQ_NOTE_RENAME:
+                event.flags = select.KQ_EV_DELETE
+                kq.control([event],0,0)
+                fh.close()
+                sleep(1)
+                fh = open(filename,"r")
+                kq.control([kevent(fh,
+                                   filter=select.KQ_FILTER_VNODE,
+                                   flags=select.KQ_EV_ADD |
+                                         select.KQ_EV_CLEAR,
+                                   fflags=select.KQ_NOTE_DELETE |
+                                          select.KQ_NOTE_RENAME |
+                                          select.KQ_NOTE_WRITE)],
+                           0
+                )
+            elif event.ident == 1000:
+                event.flags = select.KQ_EV_DISABLE
+                kq.control([event],0,0)
+except KeyboardInterrupt:
+    kq.close()
+    fh.close()
+    exit(1)
